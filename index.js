3b243669cf39d6cbf9e693ea23cea2b959e1d075
Leo Gagnon <leo@Leos-MacBook-Air.local>
2020-05-27 21:31:52 -0400

    Added basic google auth

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require('express');
-
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
+require('dotenv').config();
+require('./passport-setup');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieSession({
+  secret: 'This is the session secret',
+  resave: false,
+  saveUninitialized: true
+}));
+
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+}
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => res.send('This is the root dir'));
+app.get('/failed', (req, res) => res.send('You failed to log in!'));
+app.get('/home', isLoggedIn, (req, res) => res.send(`<h1>Welcome ${req.user.displayName}`));
+app.get('/auth', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get('/auth/callback',
+  passport.authenticate('google', { failureRedirect: '/failed' }),
+  (req, res) => res.redirect('/home'),
+);
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ passport-setup.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({ clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: '/auth/callback'
+},
+  (accessToken, refreshToken, profile, done) => {
+    return done(null, profile); //TODO This function needs to use database, like below
+    //User.findOrCreate({ googleId: profile.id }, (err, user) => return done(err, user));
+  }
+));
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user); //This might need to be user.id
+});
+
